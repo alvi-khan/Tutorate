99d791b81e848f35e3811055fe62baf8f1a443c5
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2023-05-08 10:29:51 +0600

    Updated WebMvcConfigurer.

--- Back-End/src/main/java/com/example/tutorate/WebConfig.java
+++ Back-End/src/main/java/com/example/tutorate/WebConfig.java
@@ -1,37 +1,27 @@
 package com.example.tutorate;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
     @Value("${server.origin}")
     private String origin;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(origin)
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/",
-            "classpath:/resources/",
-            "classpath:/static/",
-            "classpath:/public/"
-    };
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurer() {
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**")
+                       .allowedOrigins(origin)
+                       .allowedMethods("*")
+                       .allowedHeaders("*")
+                       .allowCredentials(true)
+                       .maxAge(3600);
+           }
+       };
     }
 }
